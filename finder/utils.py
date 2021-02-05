6a08e1f61720a1525fa052b0876fd3a30984cf79
justinschuster <schujustin@gmail.com>
2021-02-05 15:44:09 -0500

    Gets real ip address

--- finder/utils.py
+++ finder/utils.py
@@ -1,11 +1,22 @@
 import requests
 
-def get_location():
+def get_client_ip(request):
+    """ Gets the clients ip address. """
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[-1]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+def get_location(ip):
+    """ Gets the location based on the ip address """
     resp = requests.get('http://freegeoip.app/json/')
     geodata = resp.json()
     return geodata['latitude'], geodata['longitude']
 
 def get_barbers(latitude, longitude):
+    """ Returns barbers within 25 miles on latitude and longitude """
     yelp_api_key = 'dbJGI1ZLSlJTRp5idMBO6w3_eO7BVElXPoym3jSheoeopQUAugdmaK-VXVeFBvGXAGhHTrjV3DRd6bbbPTeLve90b7osiAvdYLCqw7M9YTb4Ch5IkFDEJ0_7oYIdYHYx' 
     headers = {'Authorization': f'Bearer {yelp_api_key}'}
     yelp_response = requests.get(f'https://api.yelp.com/v3/businesses/search?categories=menshair&latitude={latitude}&longitude={longitude}&sort_by=rating', headers=headers)
--- finder/views.py
+++ finder/views.py
@@ -2,13 +2,14 @@ import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from finder.utils import get_location, get_barbers
+from finder.utils import get_location, get_barbers, get_client_ip
 from .models import Greeting
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    latitude, longitude = get_location()
+    ip = get_client_ip(request)
+    latitude, longitude = get_location(ip)
     businesses = get_barbers(latitude, longitude)['businesses']
     return render(request, "index.html", {'business': businesses})
 
